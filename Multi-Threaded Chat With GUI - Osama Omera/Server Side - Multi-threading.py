2404c09d8213426686b67c4496c5cefd36c1f0c7
Osamaomera <92927320+Osamaomera@users.noreply.github.com>
2023-05-31 00:40:00 +0300

    Update Server Side - Multi-threading.py

--- Multi-Threaded Chat With GUI - Osama Omera/Server Side - Multi-threading.py
+++ Multi-Threaded Chat With GUI - Osama Omera/Server Side - Multi-threading.py
@@ -1,38 +1,53 @@
-
-
-from socket import *                             # for networking  
-from _thread import *                            #to use threads for every client
-
-
-host = '127.0.0.1'                               #loop back address
-port = 12221                                     #pick any free port on the computer 
+'Chat Room Connection - Client-To-Client'
+import threading
+import socket
+host = '127.0.0.1'
+port = 59000
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
 clients = []
+aliases = []
 
-sock = socket((AF_INET), SOCK_STREAM)            #create socket object with socket diagram type using udp
-sock.bind((host, port))                          #bind to host and port
-sock.listen(20)                                  #listen for connections
-print("server is running...")
-print("server ip is " + host)
-print("server port is "+ str(port))
 
-                                                #define of recieving behavior
-def recieving(conn , addr):
-    while True:
-        msg = str(addr[1]) + "-> " + conn.recv(2048).decode()
-        
-        for client in clients:
-            if client != conn:
-                client.send(msg.encode())
-
-while True:
-    conn, addr = sock.accept()
-    print(str(addr[1]) + " joined the room " )
-    msg = "connection from "+ str(addr[1]) + " established \n"
+def broadcast(message):
     for client in clients:
-            client.send(msg.encode())
-    clients.append(conn)
-    start_new_thread(recieving, (conn, addr))
-    
+        client.send(message)
+
+# Function to handle clients'connections
 
 
+def handle_client(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            alias = aliases[index]
+            broadcast(f'{alias} has left the chat room!'.encode('utf-8'))
+            aliases.remove(alias)
+            break
+# Main function to receive the clients connection
 
+
+def receive():
+    while True:
+        print('Server is running and listening ...')
+        client, address = server.accept()
+        print(f'connection is established with {str(address)}')
+        client.send('alias?'.encode('utf-8'))
+        alias = client.recv(1024)
+        aliases.append(alias)
+        clients.append(client)
+        print(f'The alias of this client is {alias}'.encode('utf-8'))
+        broadcast(f'{alias} has connected to the chat room'.encode('utf-8'))
+        client.send('you are now connected!'.encode('utf-8'))
+        thread = threading.Thread(target=handle_client, args=(client,))
+        thread.start()
+
+
+if __name__ == "__main__":
+    receive()
